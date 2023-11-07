f1353312a7ce6ffaee04b55eb9ef41487ffb93d0
Max Pog <pogorelovmaxim@gmail.com>
2023-11-07 17:43:13 +0200

    add first lesson

---
+++ js/script.js
@@ -0,0 +1,26 @@
+import { convertStringNumber } from "./convertStringNumber.js";
+
+const financeForm = document.querySelector('.finance__form');
+const financeAmount = document.querySelector('.finance__amount');
+
+let amount = 0;
+
+financeAmount.textContent = amount;
+
+financeForm.addEventListener('submit', (e) => {
+    event.preventDefault();
+    
+    const typeOperation = e.submitter.dataset.typeOperation;
+    
+    const changeAmount = Math.abs(convertStringNumber(financeForm.amount.value));
+
+    if (typeOperation === 'income') {
+        amount += changeAmount;
+    }
+
+    if (typeOperation === 'expenses') {
+        amount -= changeAmount;
+    }
+
+    financeAmount.textContent = `${amount.toLocaleString()} ₽`;
+});
